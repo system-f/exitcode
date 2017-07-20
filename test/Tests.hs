da43945a351f9cb760b9b7e7066832de23aae071
Andrew McCluskey <andrew@ajmccluskey.com>
2017-07-20 16:21:45 +1000

    Use NonZero type in properties + add tests

--- test/Tests.hs
+++ test/Tests.hs
@@ -1,9 +1,10 @@
-import           Control.Lens             (review)
+import           Control.Lens             (review, (^.))
 import           Data.Functor.Classes     (Eq1)
+import           Data.Functor.Identity    (runIdentity)
 import           Data.Monoid              ((<>))
 
-import           Test.QuickCheck          (Arbitrary (..))
-import           Test.QuickCheck.Checkers (TestBatch, Test, EqProp (..), eq)
+import           Test.QuickCheck          (Arbitrary (..), suchThat)
+import           Test.QuickCheck.Checkers (EqProp (..), Test, TestBatch, eq)
 import           Test.QuickCheck.Classes  (applicative, functor)
 import           Test.Tasty               (TestTree, defaultMain, testGroup)
 import           Test.Tasty.HUnit         (testCase, (@?=))
@@ -11,7 +12,9 @@ import           Test.Tasty.QuickCheck    (testProperty)
 
 import           Control.Exitcode         (Exitcode, ExitcodeT, exitfailure0,
                                            exitsuccess, exitsuccess0,
-                                           _ExitFailure)
+                                           runExitcode, _ExitFailure, exitCode)
+
+import           System.Exit             (ExitCode (..))
 
 newtype EW f a = EW { unEW :: ExitcodeT f a } deriving (Eq, Show)
 
@@ -29,6 +32,11 @@ instance (Eq1 f, Eq a) => EqProp (EW f a) where
   (=-=) = eq
 
 type CheckMe = EW [] (Integer, Integer, Integer)
+
+newtype NonZero = NonZero Int deriving Show
+
+instance Arbitrary NonZero where
+  arbitrary = NonZero <$> suchThat arbitrary (/= 0)
 
 main :: IO ()
 main = defaultMain test_Exitcode
@@ -52,8 +60,20 @@ applicativeTest =
 noLeft0Test :: TestTree
 noLeft0Test =
   testGroup "Left 0 is impossible" [
-    testProperty "_ExitFailure does not produce `Left 0`" $
-      \n -> liftA2 bool (== exitfailure0 n) (== exitsuccess0) (review _ExitFailure n) (n == 0)
+    testProperty "_ExitFailure for non-zero input" $
+      \(NonZero n) -> review _ExitFailure n == exitfailure0 n
+  , testCase "_ExitFailure 0" $
+      review _ExitFailure 0 @?= (exitsuccess0)
+
+  , testProperty "exitfailure0 with non-zero input" $
+      \(NonZero n) -> (runIdentity . runExitcode) (exitfailure0 n) == Left n
+  , testCase "exitfailure0 0" $
+      (runIdentity . runExitcode) (exitfailure0 0) @?= Right ()
+
+  , testProperty "exitCode Iso with non-zero input" $
+      \(NonZero n) -> runIdentity (exitfailure0 n ^. exitCode) == ExitFailure n
+  , testCase "exitCode Iso 0" $
+      runIdentity (exitfailure0 0 ^. exitCode) @?= ExitSuccess
   ]
 
 tastyCheckersBatch :: TestBatch -> TestTree
