60cb7952ddd8f2d8567e3ea4738d084f71b8179f
Tony Morris <tmorris@tmorris.net>
2021-12-02 18:47:15 +1000

    update exitcode

--- src/Control/Exitcode.hs
+++ src/Control/Exitcode.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE LambdaCase            #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TupleSections         #-}
@@ -10,8 +11,11 @@ module Control.Exitcode (
                         -- * Types
                           ExitcodeT
                         , Exitcode
+                        , Exitcode'
                         , ExitcodeT0
+                        , ExitcodeT0'
                         , Exitcode0
+                        , Exitcode0'
                         -- * Construction
                         , exitsuccess
                         , exitsuccess0
@@ -19,17 +23,21 @@ module Control.Exitcode (
                         , fromExitCode
                         , fromExitCode'
                         -- * Extraction
+                        , runExitcodeT
                         , runExitcode
                         -- * Optics
                         , exitCode
+                        , liftIso
                         , _ExitFailure
+                        , _ExitFailure'
+                        , _ExitFailureE
                         , _ExitSuccess
                         ) where
 
 import           Control.Applicative        (Applicative(pure, (<*>)), liftA2)
 import           Control.Category           ((.))
-import           Control.Lens               (Iso, Prism', Prism, iso, prism', prism, view, over,
-                                             (^?), _Left)
+import           Control.Lens               (Iso, Prism, Traversal', Traversal, iso, prism, view, over,
+                                             _Left, _1, _2)
 import           Control.Monad              (Monad(return, (>>=)))
 import           Control.Monad.Cont.Class   (MonadCont (..))
 import           Control.Monad.Error.Class  (MonadError (..))
@@ -59,7 +67,7 @@ import           Data.Functor.Classes       (Eq1, Ord1, Show1, compare1, eq1,
                                              showsPrec1, showsUnary1)
 # endif
 import           Data.Functor.Extend        (Extend, duplicated)
-import           Data.Functor.Identity      (Identity (Identity))
+import           Data.Functor.Identity      (Identity (Identity, runIdentity))
 import           Data.Int                   (Int)
 import           Data.Maybe                 (Maybe(Just, Nothing), fromMaybe)
 import           Data.Ord                   (Ord(compare))
@@ -73,30 +81,39 @@ import           System.Exit                (ExitCode (ExitFailure, ExitSuccess)
 -- | An exit code status where failing with a value `0` cannot be represented.
 --
 -- Transformer for either a non-zero exit code (`Int`) or a value :: `a`.
-data ExitcodeT f a =
-  ExitcodeT (f (Either Int a))
+newtype ExitcodeT e f a =
+  ExitcodeT (f (Either (e, Int) a))
 
-type Exitcode a =
-  ExitcodeT Identity a
+type Exitcode e a =
+  ExitcodeT e Identity a
 
-type ExitcodeT0 f =
-  ExitcodeT f ()
+type Exitcode' a =
+  Exitcode () a
 
-type Exitcode0 =
-  Exitcode ()
+type ExitcodeT0 f e =
+  ExitcodeT e f ()
+
+type ExitcodeT0' f =
+  ExitcodeT0 f ()
+
+type Exitcode0 e =
+  Exitcode e ()
+
+type Exitcode0' =
+  Exitcode' ()
 
 -- | Construct a succeeding exit code with the given value.
 exitsuccess ::
   Applicative f =>
   a
-  -> ExitcodeT f a
+  -> ExitcodeT e f a
 exitsuccess =
   ExitcodeT . pure . Right
 
 -- | Construct a succeeding exit code with unit.
 exitsuccess0 ::
   Applicative f =>
-  ExitcodeT0 f
+  ExitcodeT0 f e
 exitsuccess0 =
   exitsuccess ()
 
@@ -105,72 +122,100 @@ exitsuccess0 =
 -- If the given status is `0` then the exit code will succeed with unit.
 exitfailure0 ::
   Applicative f =>
-  Int
-  -> ExitcodeT0 f
-exitfailure0 n =
+  e
+  -> Int
+  -> ExitcodeT0 f e
+exitfailure0 e n =
   if n == 0
     then
       exitsuccess0
     else
-      ExitcodeT . pure . Left $ n
-
-fromExitCode ::
-  Functor f =>
-  f ExitCode
-  -> ExitcodeT0 f
-fromExitCode x =
-  let ExitcodeT (MaybeT r) = view exitCode x
-  in  ExitcodeT (fromMaybe (Right ()) <$> r)
-
-fromExitCode' ::
-  ExitCode
-  -> Exitcode0
-fromExitCode' =
-  fromExitCode . Identity
+      ExitcodeT . pure . Left $ (e, n)
 
 exitCode ::
   (Functor f, Functor g) =>
   Iso
     (f ExitCode)
     (g ExitCode)
-    (ExitcodeT0 (MaybeT f))
-    (ExitcodeT0 (MaybeT g))
+    (ExitcodeT0' (MaybeT f))
+    (ExitcodeT0' (MaybeT g))
 exitCode =
   iso
-    (\x -> ExitcodeT (MaybeT ((\e ->  case e of
+    (\x -> ExitcodeT (MaybeT ((\case
                                         ExitSuccess ->
                                           Just (Right ())
                                         ExitFailure 0 ->
                                           Nothing
                                         ExitFailure n ->
-                                          Just (Left n)) <$> x)))
-    (\(ExitcodeT (MaybeT x)) -> (\e ->  case e of
+                                          Just (Left ((), n))) <$> x)))
+    (\(ExitcodeT (MaybeT x)) -> (\case
                                           Just (Right ()) ->
                                             ExitSuccess
                                           Nothing ->
                                             ExitFailure 0
-                                          Just (Left n) ->
+                                          Just (Left ((), n)) ->
                                             ExitFailure n) <$> x)
 
-runExitcode ::
-  ExitcodeT f a
-  -> f (Either Int a)
-runExitcode (ExitcodeT x) =
+fromExitCode ::
+  Functor f =>
+  f ExitCode
+  -> ExitcodeT0' f
+fromExitCode x =
+  let ExitcodeT (MaybeT r) = view exitCode x
+  in  ExitcodeT (fromMaybe (Right ()) <$> r)
+
+fromExitCode' ::
+  ExitCode
+  -> Exitcode0'
+fromExitCode' =
+  fromExitCode . Identity
+
+runExitcodeT ::
+  ExitcodeT e f a
+  -> f (Either (e, Int) a)
+runExitcodeT (ExitcodeT x) =
   x
 
+runExitcode ::
+  Exitcode e a
+  -> Either (e, Int) a
+runExitcode =
+  runIdentity . runExitcodeT
+
 _ExitFailure ::
-  Prism'
-    Exitcode0
-    Int
+  Prism
+    (Exitcode0 e)
+    (Exitcode0 e')
+    (e, Int)
+    (e', Int)
 _ExitFailure =
-  prism'
-    exitfailure0
-    (\(ExitcodeT (Identity x)) -> x ^? _Left)
+  prism
+    (uncurry exitfailure0)
+    (\(ExitcodeT (Identity x)) ->
+      case x of
+        Left (e, n) -> Right (e, n)
+        Right () -> Left (ExitcodeT (Identity (Right ()))))
+
+_ExitFailure' ::
+  Traversal'
+    Exitcode0'
+    Int
+_ExitFailure' =
+  _ExitFailure . _2
+
+_ExitFailureE ::
+  Traversal
+    (Exitcode0 e)
+    (Exitcode0 e')
+    e
+    e'
+_ExitFailureE =
+  _ExitFailure . _1
 
 _ExitSuccess ::
   Prism
-    (Exitcode a)
-    (Exitcode b)
+    (Exitcode e a)
+    (Exitcode e b)
     a
     b
 _ExitSuccess =
@@ -180,48 +225,60 @@ _ExitSuccess =
       over _Left (ExitcodeT . Identity . Left) x
     )
 
-instance Functor f => Functor (ExitcodeT f) where
+liftIso ::
+  (Functor f, Functor g) =>
+  Iso
+    (ExitcodeT e f a)
+    (ExitcodeT e' g b)
+    (f (Exitcode e a))
+    (g (Exitcode e' b))
+liftIso =
+  iso
+    (\(ExitcodeT x) -> fmap (ExitcodeT . Identity) x)
+    (ExitcodeT . fmap runExitcode)
+
+instance Functor f => Functor (ExitcodeT e f) where
   fmap f (ExitcodeT x) =
     ExitcodeT (fmap (fmap f) x)
 
-instance Apply f => Apply (ExitcodeT f) where
+instance Apply f => Apply (ExitcodeT e f) where
   ExitcodeT f <.> ExitcodeT a =
     ExitcodeT (liftF2 (<.>) f a)
 
-instance Applicative f => Applicative (ExitcodeT f) where
+instance Applicative f => Applicative (ExitcodeT e f) where
   pure =
     ExitcodeT . pure . pure
   ExitcodeT f <*> ExitcodeT a =
     ExitcodeT (liftA2 (<*>) f a)
 
-instance (Bind f, Monad f) => Bind (ExitcodeT f) where
+instance (Bind f, Monad f) => Bind (ExitcodeT e f) where
   (>>-) =
     (>>=)
 
-instance Monad f => Monad (ExitcodeT f) where
+instance Monad f => Monad (ExitcodeT e f) where
   return =
     ExitcodeT . return . return
   ExitcodeT x >>= f =
     ExitcodeT
       (x >>= either (pure . Left) (\a -> let ExitcodeT y = f a in y))
 
-instance Monad f => Alt (ExitcodeT f) where
+instance Monad f => Alt (ExitcodeT e f) where
   ExitcodeT a <!> ExitcodeT b =
     ExitcodeT (a >>= either (const b) (pure a))
 
-instance Monad f => Semigroup (ExitcodeT f a) where
+instance Monad f => Semigroup (ExitcodeT e f a) where
   ExitcodeT a <> ExitcodeT b =
     ExitcodeT (a >>= either (const b) (pure a))
 
-instance Applicative f => Extend (ExitcodeT f) where
+instance Applicative f => Extend (ExitcodeT e f) where
   duplicated (ExitcodeT x) =
     ExitcodeT ((pure <$>) <$> x )
 
-instance (Eq1 f, Eq a) => Eq (ExitcodeT f a) where
+instance (Eq1 f, Eq e, Eq a) => Eq (ExitcodeT e f a) where
   ExitcodeT a == ExitcodeT b =
     a `eq1` b
 
-instance Eq1 f => Eq1 (ExitcodeT f) where
+instance (Eq e, Eq1 f) => Eq1 (ExitcodeT e f) where
 # if MIN_VERSION_transformers(0,5,0)
   liftEq f (ExitcodeT a) (ExitcodeT b) =
     liftEq (liftEq f) a b
@@ -230,11 +287,11 @@ instance Eq1 f => Eq1 (ExitcodeT f) where
    eq1 a b
 # endif
 
-instance (Ord1 f, Ord a) => Ord (ExitcodeT f a) where
+instance (Ord1 f, Ord e, Ord a) => Ord (ExitcodeT e f a) where
   ExitcodeT a `compare` ExitcodeT b =
     a `compare1` b
 
-instance (Ord1 f) => Ord1 (ExitcodeT f) where
+instance (Ord1 f, Ord e) => Ord1 (ExitcodeT e f) where
 # if MIN_VERSION_transformers(0,5,0)
   liftCompare f (ExitcodeT a) (ExitcodeT b) =
     liftCompare (liftCompare f) a b
@@ -243,7 +300,7 @@ instance (Ord1 f) => Ord1 (ExitcodeT f) where
     compare1 a b
 # endif
 
-instance (Show1 f, Show a) => Show (ExitcodeT f a) where
+instance (Show1 f, Show e, Show a) => Show (ExitcodeT e f a) where
   showsPrec d (ExitcodeT m) =
 # if MIN_VERSION_transformers(0,5,0)
     showsUnaryWith showsPrec1 "ExitcodeT" d m
@@ -251,7 +308,7 @@ instance (Show1 f, Show a) => Show (ExitcodeT f a) where
     showsUnary1 "ExitcodeT" d m
 # endif
 
-instance Show1 f => Show1 (ExitcodeT f) where
+instance (Show e, Show1 f) => Show1 (ExitcodeT e f) where
 # if MIN_VERSION_transformers(0,5,0)
   liftShowsPrec sp sl d (ExitcodeT fa) =
     let showsPrecF = liftA2 liftShowsPrec (uncurry liftShowsPrec) (uncurry liftShowList) (sp, sl)
@@ -261,28 +318,28 @@ instance Show1 f => Show1 (ExitcodeT f) where
     showsUnary1 "ExitcodeT" d fa
 # endif
 
-instance Foldable f => Foldable (ExitcodeT f) where
+instance Foldable f => Foldable (ExitcodeT e f) where
   foldr f z (ExitcodeT x) =
     foldr (flip (foldr f)) z x
 
-instance Foldable1 f => Foldable1 (ExitcodeT f)
+instance Foldable1 f => Foldable1 (ExitcodeT e f)
 
-instance Traversable f => Traversable (ExitcodeT f) where
+instance Traversable f => Traversable (ExitcodeT e f) where
   traverse f (ExitcodeT x) =
     ExitcodeT <$> traverse (traverse f) x
 
-instance MonadIO f => MonadIO (ExitcodeT f) where
+instance MonadIO f => MonadIO (ExitcodeT e f) where
   liftIO io =
     ExitcodeT (Right <$> liftIO io)
 
-instance MonadTrans ExitcodeT where
+instance MonadTrans (ExitcodeT e) where
   lift = ExitcodeT . (>>= pure . pure)
 
-instance MonadReader r f => MonadReader r (ExitcodeT f) where
+instance MonadReader r f => MonadReader r (ExitcodeT e f) where
   ask = lift ask
   local f (ExitcodeT m) = ExitcodeT $ local f m
 
-instance MonadWriter w f => MonadWriter w (ExitcodeT f) where
+instance MonadWriter w f => MonadWriter w (ExitcodeT e f) where
   writer t = ExitcodeT . fmap pure $ writer t
   listen (ExitcodeT m) =
      ExitcodeT ((\(e, w) -> (,w) <$> e) <$> listen m)
@@ -292,35 +349,35 @@ instance MonadWriter w f => MonadWriter w (ExitcodeT f) where
     tell (f w)
     pure a
 
-instance MonadState s f => MonadState s (ExitcodeT f) where
+instance MonadState s f => MonadState s (ExitcodeT e f) where
   get = ExitcodeT (fmap Right get)
   put = ExitcodeT . fmap Right . put
 
-instance MonadError e f => MonadError e (ExitcodeT f) where
+instance MonadError e f => MonadError e (ExitcodeT e f) where
   throwError = ExitcodeT . fmap Right . throwError
   catchError (ExitcodeT f) h =
-     ExitcodeT $ flip catchError (runExitcode . h) f
+     ExitcodeT $ flip catchError (runExitcodeT . h) f
 
-instance MonadRWS r w s f => MonadRWS r w s (ExitcodeT f)
+instance MonadRWS r w s f => MonadRWS r w s (ExitcodeT e f)
 
 -- Given the embedded `Either` we can only handle computations that use `Either`.
 -- This code taken from the ExceptT instance:
 --   https://hackage.haskell.org/package/transformers-0.5.4.0/docs/src/Control.Monad.Trans.Except.html#line-237
-instance MonadCont f => MonadCont (ExitcodeT f) where
+instance MonadCont f => MonadCont (ExitcodeT e f) where
   callCC = liftCallCC callCC
 
-liftCallCC :: Functor f => (((Either Int a -> f (Either Int b)) -> f (Either Int a)) -> f (Either Int a))
-           -> ((a -> ExitcodeT f b) -> ExitcodeT f a)
-           -> ExitcodeT f a
+liftCallCC :: Functor f => (((Either (e, Int) a -> f (Either (e, Int) b)) -> f (Either (e, Int) a)) -> f (Either (e, Int) a))
+           -> ((a -> ExitcodeT e f b) -> ExitcodeT e f a)
+           -> ExitcodeT e f a
 liftCallCC callCC' f =
   ExitcodeT . callCC' $
-    \c -> runExitcode (f (\a -> ExitcodeT (c (Right a))))
+    \c -> runExitcodeT (f (ExitcodeT . c . Right))
 
-instance MFunctor ExitcodeT where
+instance MFunctor (ExitcodeT e) where
   hoist nat (ExitcodeT x) =
     ExitcodeT (nat x)
 
-instance MMonad ExitcodeT where
+instance MMonad (ExitcodeT e) where
   embed nat (ExitcodeT x) =
     let ex (Left e) = Left e
         ex (Right (Left e)) = Left e
--- src/Control/Process/Process.hs
+++ src/Control/Process/Process.hs
@@ -4,15 +4,27 @@
 module Control.Process.Process(
   module Process
 , readCreateProcessWithExitCode
+, readCreateProcessWithExitCode'
 , readProcessWithExitCode
+, readProcessWithExitCode'
 , waitForProcess
+, waitForProcess'
 , getProcessExitCode
+, getProcessExitCode'
 ) where
 
 import Control.Category ( Category((.)) )
 import Control.Exitcode
-    ( Exitcode0, ExitcodeT0, fromExitCode, fromExitCode' )
-import Control.Lens ( over, Field1(_1) )
+    ( ExitcodeT,
+      fromExitCode,
+      fromExitCode',
+      _ExitSuccess,
+      liftIso,
+      ExitcodeT0',
+      Exitcode0',
+      _ExitFailureE )
+import Control.Lens ( over, view, review, set, Field1(_1) )
+import Control.Monad.Trans.Maybe ( MaybeT(MaybeT) )
 import Data.Functor ( Functor(fmap) )
 import Data.Maybe ( Maybe )
 import Data.String ( String )
@@ -45,29 +57,56 @@ import System.Process as Process(
   )
 import qualified System.Process as P(readCreateProcessWithExitCode, readProcessWithExitCode, waitForProcess, getProcessExitCode)
 
+readCreateProcessWithExitCode' ::
+  CreateProcess
+  -> String
+  -> ExitcodeT String IO String
+readCreateProcessWithExitCode' p a =
+  review liftIso (fmap (\(x, y, z) -> set _ExitSuccess y (set _ExitFailureE z x)) (readCreateProcessWithExitCode p a))
+
 readCreateProcessWithExitCode ::
   CreateProcess
   -> String
-  -> IO (Exitcode0, String, String)
+  -> IO (Exitcode0', String, String)
 readCreateProcessWithExitCode p a =
-   fmap (over _1 fromExitCode') (P.readCreateProcessWithExitCode p a)
+  fmap (over _1 fromExitCode') (P.readCreateProcessWithExitCode p a)
+
+readProcessWithExitCode' ::
+  FilePath
+  -> [String]
+  -> String
+  -> ExitcodeT String IO String
+readProcessWithExitCode' p a i =
+  review liftIso (fmap (\(x, y, z) -> set _ExitSuccess y (set _ExitFailureE z x)) (readProcessWithExitCode p a i))
 
 readProcessWithExitCode ::
   FilePath
   -> [String]
   -> String
-  -> IO (Exitcode0, String, String)
+  -> IO (Exitcode0', String, String)
 readProcessWithExitCode p a i =
-   fmap (over _1 fromExitCode') (P.readProcessWithExitCode p a i)
+  fmap (over _1 fromExitCode') (P.readProcessWithExitCode p a i)
 
 waitForProcess ::
   ProcessHandle
-  -> IO Exitcode0
+  -> IO Exitcode0'
 waitForProcess =
   fmap fromExitCode' . P.waitForProcess
 
+waitForProcess' ::
+  ProcessHandle
+  -> ExitcodeT0' IO
+waitForProcess' =
+  review liftIso . waitForProcess
+
 getProcessExitCode ::
   ProcessHandle
-  -> IO (ExitcodeT0 Maybe)
+  -> IO (ExitcodeT0' Maybe)
 getProcessExitCode =
   fmap fromExitCode . P.getProcessExitCode
+
+getProcessExitCode' ::
+  ProcessHandle
+  -> ExitcodeT0' (MaybeT IO)
+getProcessExitCode' =
+  review liftIso . MaybeT . fmap (view liftIso) . getProcessExitCode
